6748daa946093fbfd4ca1e294bd326766ef1486e
Carla H <hetterichcarla@gmail.com>
2024-07-09 20:59:03 +0200

    desafio 2

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -16,11 +16,26 @@ alert('Boas vindas ao jogo do número secreto.');
 
 let numeroSecreto = 9;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10.');
+let chute;
+let tentativas = 1;
 
+//enquanto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10.');
+    //se
+    if (numeroSecreto == chute) {
+        alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+    } else { 
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}.`); 
+        } else {
+            alert(`O número secreto é maior que ${chute}.`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 
-if (numeroSecreto == chute) {
-    alert(`Parabéns, você acertou o número secreto ${numeroSecreto} !`);
-} else { 
-    alert('Que pena, você errou o número secreto!');
 }
+
+
+
