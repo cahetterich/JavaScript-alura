20bdac1ca51067c23d9724ee1addaa2c0cf6bbb8
Carla H <hetterichcarla@gmail.com>
2024-07-10 19:22:47 +0200

    inclusão variável

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -14,14 +14,16 @@
 
 alert('Boas vindas ao jogo do número secreto.');
 
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 1000;
+
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100.');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}.`);
     //se
     if (numeroSecreto == chute) {
         break;       
