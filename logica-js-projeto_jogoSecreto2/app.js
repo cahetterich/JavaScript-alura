b95f779c15f15dd58e439f8c56ecd2f9fa8bc693
Carla H <hetterichcarla@gmail.com>
2024-07-19 22:40:16 +0200

    contador js

--- logica-js-projeto_jogoSecreto2/app.js
+++ logica-js-projeto_jogoSecreto2/app.js
@@ -1,6 +1,7 @@
 
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {     
     let campo = document.querySelector(tag);
@@ -13,16 +14,20 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 function verificarChute() {
     let chute = document.querySelector('input').value;
    // console.log(chute == numeroSecreto); // boolean - false o true
-
+    
    if (chute == numeroSecreto) {
-    exibirTextoNaTela('h1', 'Acertou!');
-    exibirTextoNaTela('p', 'Parabéns, você acertou o número secreto!');
+    exibirTextoNaTela('h1', 'Acertou!'); 
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Parabéns, você acertou o número secreto com ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela('p', mensagemTentativas);
    } else {
         if ( chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor!');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior!');
-        }   
+        }  
+        // tentativas = tentativas + 1; 
+        tentativas++;
    }
 }
 
