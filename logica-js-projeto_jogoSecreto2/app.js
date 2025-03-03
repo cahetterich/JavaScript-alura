c62a32df642aadaa7cfc9f8c1b3adb9fb0341d20
Carla H <hetterichcarla@gmail.com>
2025-03-03 23:57:08 +0100

    Web Speech API

--- logica-js-projeto_jogoSecreto2/app.js
+++ logica-js-projeto_jogoSecreto2/app.js
@@ -11,6 +11,22 @@ function exibirTextoNaTela(tag, texto) {
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
+// Web Speech API (leitor de texto) - sem utilizar o responsiveVoice e sem necessidade de script
+
+// function exibirTextoNaTela(tag, texto) {
+//     let campo = document.querySelector(tag);
+//     campo.innerHTML = texto;
+//     if ('speechSynthesis' in window) {
+//         let utterance = new SpeechSynthesisUtterance(texto);
+//         utterance.lang = 'pt-BR'; 
+//         utterance.rate = 1.2; 
+//         window.speechSynthesis.speak(utterance); 
+//     } else {
+//         console.log("Web Speech API não suportada neste navegador.");
+//     }
+// }
+
+
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
