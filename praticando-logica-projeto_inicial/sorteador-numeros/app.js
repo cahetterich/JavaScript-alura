d20c182cb18696673f96fbdba4c470aedc5503bd
Carla H <hetterichcarla@gmail.com>
2025-03-25 23:53:20 +0100

    function

---
+++ praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,16 @@
+//sorteador
+function sortear(){
+    // quantidade
+    let quantidade = document.getElementById('quantidade').value; 
+
+    // de 
+    let de = document.getElementById('de').value;
+
+    // ate
+    let ate = document.getElementById('ate').value; 
+    
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+
+}
