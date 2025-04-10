709b501e86faab677acda09a4f3ccad61c884407
Carla H <hetterichcarla@gmail.com>
2025-04-10 22:25:46 +0200

    alugames

--- praticando-logica-projeto_inicial/alugames/js/app.js
+++ praticando-logica-projeto_inicial/alugames/js/app.js
@@ -6,17 +6,20 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    // let nomeJogo = gameClicado.querySelector('.dashboard__item__name'); // teste para aparecer o nome do jogo no alerta
 
-    alert(nomeJogo.textContent);
+    // alert(nomeJogo.textContent); // testando se as informações são puxadas
 
-    // if (botao.classList.contains('.dashboard__item__button--return')) {
-    //     botao.classList.remove('.dashboard__item__button--return');
-    //     botao.classList.add('dashboard__item__button ');
+    //
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
 
-    // } else {
-    //     botao.classList.remove('dashboard__item__button ');
-    //     botao.classList.add('.dashboard__item__button--return');
-    // }
+    } else {        
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
 
 }
