c62eb90edf80033c5d522bddbf62b31253511972
Carla H <hetterichcarla@gmail.com>
2025-04-13 23:48:41 +0200

    let

--- praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -3,7 +3,9 @@
 // adicionar produto ao carrinho 
 function adicionar() {
 // recuperar valores: nome do produto; quantidade; valor
-let nomeProduto = document.getElementById('produto');
+let produto = document.getElementById('produto').value;
+let nomeProduto = ;
+let valorUnitario = ;
 let quantidade = document.getElementById('quantidade');
 // 1 TESTE - verificar se as informações estão sendo puxadas corretamente
 // alert(nomeProduto.value);
