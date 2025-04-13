2e53f2dad980d222718a2695bab27be1dacb68e8
Carla H <hetterichcarla@gmail.com>
2025-04-14 00:16:51 +0200

    section carrinho

--- praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -6,17 +6,24 @@ function adicionar() {
 let produto = document.getElementById('produto').value;
 let nomeProduto = produto.split('-')[0]; // split puxa os caracters que desejamos puxar ([0]primeira opção string que vem antes da '-')
 let valorUnitario = produto.split('R$')[1]; // slip puxa os caracteres que estão após o 'R$'
-let quantidade = document.getElementById('quantidade');
-let preco = quantidade.value * valorUnitario
-// TESTE - verificar se as informações estão sendo puxadas corretamente
+let quantidade = document.getElementById('quantidade').value;
+
+// calcular o preço de cada produto/subtotal
+let preco = quantidade * valorUnitario;
+
+// adicionar no carrinho
+let carrinho = document.getElementById('lista-produtos');
+// concatenar todos os itens no carrinho / section
+carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul"> R$${preco} </span></section>`;
+
+
+// TESTES - verificar se as informações estão sendo puxadas corretamente
 // alert(quantidade.value);
 // alert(nomeProduto);
 // alert(valorUnitario);
 // alert(preco);
 
-// calcular o preço de cada produto/subtotal
-
-// adicionar no carrinho
 
 // calcular o preço total da compra
 }
