7e5186d1ed3b7ab0f31b6fce95b3a081e9db8c6e
Carla H <hetterichcarla@gmail.com>
2025-04-13 23:58:11 +0200

    slip

--- praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -4,13 +4,15 @@
 function adicionar() {
 // recuperar valores: nome do produto; quantidade; valor
 let produto = document.getElementById('produto').value;
-let nomeProduto = ;
-let valorUnitario = ;
+let nomeProduto = produto.split('-')[0]; // split puxa os caracters que desejamos puxar ([0]primeira opção string que vem antes da '-')
+let valorUnitario = produto.split('R$')[1]; // slip puxa os caracteres que estão após o 'R$'
 let quantidade = document.getElementById('quantidade');
-// 1 TESTE - verificar se as informações estão sendo puxadas corretamente
-// alert(nomeProduto.value);
+let preco = quantidade.value * valorUnitario
+// TESTE - verificar se as informações estão sendo puxadas corretamente
 // alert(quantidade.value);
-
+// alert(nomeProduto);
+// alert(valorUnitario);
+// alert(preco);
 
 // calcular o preço de cada produto/subtotal
 
