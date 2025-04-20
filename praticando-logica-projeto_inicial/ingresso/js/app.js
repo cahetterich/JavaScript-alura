fc6c15ff88acd0e81f4779c098b9f9390fd77b2b
Carla H <hetterichcarla@gmail.com>
2025-04-21 00:03:39 +0200

    comprar ingresso

--- praticando-logica-projeto_inicial/ingresso/js/app.js
+++ praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -1,7 +1,24 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
+    let qtd = parseInt(document.getElementById('qtd').value); //parseInt - converte o texto em número - value para formulário
 
-    alert(tipo.value);
-    alert(qtd.value);
+    // alert(tipo.value);
+    // alert(qtd.value);
+
+    //condicional
+    if(tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+//PISTA
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent); //parseInt - converte o texto em número - textContent para texto
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para Pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+    }
 }
