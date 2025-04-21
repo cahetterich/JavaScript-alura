6fecdfa99ffa6c479693af0709e2e1f0f8564e3e
Carla H <hetterichcarla@gmail.com>
2025-04-21 23:41:45 +0200

    ingresso

--- praticando-logica-projeto_inicial/ingresso/js/app.js
+++ praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -8,10 +8,14 @@ function comprar() {
     //condicional
     if(tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
     }
 }
 
-//PISTA
+// PISTA
 function comprarPista(qtd) {
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent); //parseInt - converte o texto em número - textContent para texto
     if (qtd > qtdPista) {
@@ -22,3 +26,28 @@ function comprarPista(qtd) {
         alert('Compra realizada com sucesso');
     }
 }
+
+// SUPERIOR
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent); //parseInt - converte o texto em número - textContent para texto
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para Superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+// INFERIOR
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent); //parseInt - converte o texto em número - textContent para texto
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para Inferior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
